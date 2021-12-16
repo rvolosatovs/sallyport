f7b32fd40ef3a208a9d4e279d2d5143e343175ea
Roman Volosatovs <roman@profian.com>
2021-12-16 10:48:56 +0100

    v2/host: Prefix unused variables
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- src/v2/src/host/mod.rs
+++ src/v2/src/host/mod.rs
@@ -155,8 +155,8 @@ fn execute_item(ptr: *mut [u8]) -> Option<*mut [u8]> {
         kind if kind == item::Kind::End as _ => return None,
 
         kind if kind == item::Kind::Syscall as _ => {
-            let size = size.checked_sub(9 * size_of::<usize>())?;
-            let (num, ptr) = unsafe { read_first(ptr) };
+            let _size = size.checked_sub(9 * size_of::<usize>())?;
+            let (num, _ptr) = unsafe { read_first(ptr) };
             match num {
                 _ => (),
             }
